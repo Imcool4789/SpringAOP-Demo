367209b305fdf241cd622dba7e115c63d8309c26
Vis Kirubakaran <72267229+vikiru@users.noreply.github.com>
2022-03-18 11:53:24 -0400

    Add files via upload

--- src/main/java/com/group5/AOPDemo/Main.java
+++ src/main/java/com/group5/AOPDemo/Main.java
@@ -1,18 +1,16 @@
 package com.group5.AOPDemo;
 
-import com.group5.AOPDemo.BuddyInfoService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        BuddyInfoService buddyService = context.getBean("buddyService",BuddyInfoService.class);
+        BuddyInfoService buddyService = context.getBean("buddyInfoService",BuddyInfoService.class);
         System.out.println("Name: " + buddyService.getBuddyInfo().getName());
         System.out.println("Address: " + buddyService.getBuddyInfo().getAddress());
         System.out.println("Phone Number: " + buddyService.getBuddyInfo().getPhone_num());
         buddyService.getBuddyInfo().setName("Mark");
         buddyService.getBuddyInfo().setAddress("123 Beach Dr");
         buddyService.getBuddyInfo().setPhone_num("555-555-5555");
-        context.close();
     }
 }
