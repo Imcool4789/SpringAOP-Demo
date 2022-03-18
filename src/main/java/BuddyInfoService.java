8620eb835c37a09fe557210de05248bf57fdfd05
Kashish708 <s.kashish0123@gmail.com>
2022-03-18 10:06:39 -0400

    Added BuddyInfo Service and JoinPoint

---
+++ src/main/java/BuddyInfoService.java
@@ -0,0 +1,12 @@
+public class BuddyInfoService {
+
+    private BuddyInfo buddy;
+
+    public BuddyInfo getBuddyInfo(){
+        return this.buddy;
+    }
+
+    public void setBuddyInfo(BuddyInfo buddy){
+        this.buddy = buddy;
+    }
+}
