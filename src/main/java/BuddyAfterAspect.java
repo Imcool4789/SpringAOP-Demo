bcff71822587b7cb4b384d0eb6102bd1318b7543
Vis Kirubakaran <72267229+vikiru@users.noreply.github.com>
2022-03-18 10:34:46 -0400

    Add files via upload

---
+++ src/main/java/BuddyAfterAspect.java
@@ -0,0 +1,35 @@
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class BuddyAfterAspect
+{
+    @After("args(name)")
+    public void logStringArguments(String name){
+        System.out.println("Running After Advice. String argument passed="+name);
+    }
+
+    @AfterThrowing("within(BuddyInfo)")
+    public void logExceptions(JoinPoint joinPoint){
+        System.out.println("Exception thrown in BuddyInfo Method="+joinPoint.toString());
+    }
+
+    @AfterReturning(pointcut="execution(* getName())", returning="returnString")
+    public void getNameReturningAdvice(String returnString){
+        System.out.println("getNameReturningAdvice executed. Returned String="+returnString);
+    }
+
+    @AfterReturning(pointcut="execution(* getAddress())", returning="returnString")
+    public void getAddressReturningAdvice(String returnString){
+        System.out.println("getAddressReturningAdvice executed. Returned String="+returnString);
+    }
+
+    @AfterReturning(pointcut="execution(* getPhone_num())", returning="returnString")
+    public void getPhone_numReturningAdvice(String returnString){
+        System.out.println("getPhone_numReturningAdvice executed. Returned String="+returnString);
+    }
+
+}
